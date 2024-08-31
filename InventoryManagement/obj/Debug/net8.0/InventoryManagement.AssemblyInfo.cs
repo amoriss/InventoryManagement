dc2fd568faeb1f57c9ad37c413de2abb5542cd53
Amoriss <amoriss@protonmail.com>
2024-08-31 00:29:41 -0500

    adjusted css so the link is the full width of the box

--- InventoryManagement/obj/Debug/net8.0/InventoryManagement.AssemblyInfo.cs
+++ InventoryManagement/obj/Debug/net8.0/InventoryManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cad409b526a9059e67d5db343691b9d356b23ec1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+717a95f368cab9101cd6261e30d3f2d3cc7c9392")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
