295d5a72611782c4aff5fb397e0ad8dbea0e7929
Amoriss <amoriss@protonmail.com>
2024-09-01 00:04:10 -0500

    added update and delete buttons with updated confirmation dialog for the delete button

--- InventoryManagement/wwwroot/js/site.js
+++ InventoryManagement/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// JavaScript to handle row click events and redirect to URL
+document.addEventListener('DOMContentLoaded', function () {
+    const rows = document.querySelectorAll('.clickable-row');
+
+    rows.forEach(row => {
+        row.addEventListener('click', function () {
+            window.location.href = this.getAttribute('data-href');
+        });
+    });
+});
