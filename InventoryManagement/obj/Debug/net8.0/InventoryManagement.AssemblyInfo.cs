094ac52640d4b538a7e695872427ee8fae74b08e
Amoriss <amoriss@protonmail.com>
2024-09-05 00:24:38 -0500

    text in view all products table changes green on hover

--- InventoryManagement/obj/Debug/net8.0/InventoryManagement.AssemblyInfo.cs
+++ InventoryManagement/obj/Debug/net8.0/InventoryManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8780147fe11a24c10b30f55608e1292da0d2421")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d30b9ed78916120e035ce0146d05f3810ab7db81")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
